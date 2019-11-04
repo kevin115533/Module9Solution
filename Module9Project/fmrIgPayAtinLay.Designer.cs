b07a944fc2400ec994f6ff8c46393daf6110197a
Kevin Tran <kevin115533@yahoo.com>
2019-11-04 15:08:08 -0600

    Pig latin translator

---
+++ Module9Project/fmrIgPayAtinLay.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Module9Project
+{
+    partial class fmrIgPayAtinLay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblUserMessage = new System.Windows.Forms.Label();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.lblTranslatedMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(529, 106);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(144, 48);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(78, 106);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(144, 48);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 43);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(776, 26);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // lblUserMessage
+            // 
+            this.lblUserMessage.AutoSize = true;
+            this.lblUserMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserMessage.Location = new System.Drawing.Point(12, 9);
+            this.lblUserMessage.Name = "lblUserMessage";
+            this.lblUserMessage.Size = new System.Drawing.Size(210, 20);
+            this.lblUserMessage.TabIndex = 3;
+            this.lblUserMessage.Text = "Enter your message here";
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTranslate.Location = new System.Drawing.Point(306, 106);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(144, 48);
+            this.btnTranslate.TabIndex = 4;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // lblTranslatedMessage
+            // 
+            this.lblTranslatedMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblTranslatedMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTranslatedMessage.Location = new System.Drawing.Point(-8, 195);
+            this.lblTranslatedMessage.Name = "lblTranslatedMessage";
+            this.lblTranslatedMessage.Size = new System.Drawing.Size(816, 122);
+            this.lblTranslatedMessage.TabIndex = 5;
+            this.lblTranslatedMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fmrIgPayAtinLay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 315);
+            this.Controls.Add(this.lblTranslatedMessage);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.lblUserMessage);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.KeyPreview = true;
+            this.Name = "fmrIgPayAtinLay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IgPay AtinLay";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fmrIgPayAtinLay_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblUserMessage;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.Label lblTranslatedMessage;
+    }
+}
+
---
+++ Module9Project/fmrIgPayAtinLay.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module9Project
+{
+    public partial class fmrIgPayAtinLay : Form
+    {
+        public fmrIgPayAtinLay()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMessage.Text = "";
+            lblTranslatedMessage.Text = "";
+            txtMessage.Focus();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            string userMessage = txtMessage.Text;
+
+            try
+            {
+                if (validateData())
+                {
+                    lblTranslatedMessage.Text = userMessage;
+                }
+            }
+            catch(Exception ex)
+            {
+                lblTranslatedMessage.Text = ex.Message + ex.GetType().ToString();
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void fmrIgPayAtinLay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        public bool validateData()
+        {
+            return
+                isBlank(txtMessage) &&
+                isText(txtMessage);
+        }
+
+        private bool isText(TextBox txtMessage)
+        {
+            if (txtMessage.Text == "")
+            {
+                lblTranslatedMessage.Text = "Entry box is blank, please enter a message";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool isBlank(TextBox txtMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
